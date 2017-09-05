49b48aef05eaa32974e2822572b329d03558ed2d
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-05 22:00:56 +0200

    Poging tot functie schrijven voor checkbox (code uitgecomment)

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset1/MainActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset1/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.kimgo.kimgouweleeuw_pset1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private CheckBox arms;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    public void addBodyPart(View view) {
+//        arms = (CheckBox) findViewById(R.id.arms);
+//        arms.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (((CheckBox) v).isChecked()) {
+//
+//                }
+//            }
+//        });
+//    }
 }
