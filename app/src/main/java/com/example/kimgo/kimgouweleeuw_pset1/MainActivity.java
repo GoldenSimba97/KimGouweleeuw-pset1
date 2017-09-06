15dd934cca29accef05abe1be515e237fca82486
GoldenSimba97 <kim.gouweleeuw@gmail.com>
2017-09-06 15:10:01 +0200

    Mr. Potato Head aangekleed voor portrait

--- app/src/main/java/com/example/kimgo/kimgouweleeuw_pset1/MainActivity.java
+++ app/src/main/java/com/example/kimgo/kimgouweleeuw_pset1/MainActivity.java
@@ -10,21 +10,32 @@ import android.widget.CheckBox;
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox arms;
+    private Integer images[] = {R.drawable.arms, R.drawable.ears, R.drawable.eyebrows,
+            R.drawable.eyes, R.drawable.glasses, R.drawable.hat, R.drawable.mouth,
+            R.drawable.mustache, R.drawable.nose, R.drawable.shoes};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-//    public void addBodyPart(View view) {
-//        arms = (CheckBox) findViewById(R.id.arms);
-//        arms.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (((CheckBox) v).isChecked()) {
+
+    public void addBodyPart(View view) {
+        arms = (CheckBox) findViewById(R.id.arms);
+        arms.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (arms.isChecked()) {
+
+                }
+            }
+        });
+    }
+
+//    private void addImage() {
 //
-//                }
-//            }
-//        });
+//        final ImageView imageView = (ImageView) findViewById(R.id.imageDisplay);
+//        imageView.setImageResource(images[currImage]);
+//
 //    }
 }
